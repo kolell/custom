4756626b1bedb53e702a544de4e42a1a4d3cda98
kole <kole9402@gmail.com>
2018-06-09 22:45:47 +0800

    Spring MVC全局异常拦截

--- src/main/java/com/mmall/common/RedisPool.java
+++ src/main/java/com/mmall/common/RedisPool.java
@@ -50,13 +50,4 @@ public class RedisPool {
         pool.returnResource(jedis);
     }
 
-    public static void main(String[] args) {
-        Jedis jedis = pool.getResource();
-        jedis.set("test","testValue");
-        returnResource(jedis);
-
-        pool.destroy();//临时调用，销毁连接池中的所有连接
-        System.out.println("program is end");
-    }
-
 }
